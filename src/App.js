76331218d44884c626aacef8cd68932c6078214c
MohammadAminAkbari <akbariamin01@gmail.com>
2023-03-02 15:26:56 +0330

    Every thing Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
-function App() {
+import Home from './pages/Home'
+import Product from './pages/Product'
+
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={Product} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
